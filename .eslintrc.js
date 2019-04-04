fb713f47534b7af862c6ad26bad59df9835cb2b5
SirSerje <sir.serje@gmail.com>
2019-04-04 15:35:34 +0300

    #1 separate eslint rules to file, update linting

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+    'jest': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'space-infix-ops': [
+      2,
+      {
+        'int32Hint': false
+      }
+    ],
+    'no-console': 'warn',
+    'no-alert': 'warn',
+    'comma-spacing': [
+      'error',
+      {
+        'before': false,
+        'after': true
+      }
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'never',
+        'objects': 'never',
+        'imports': 'never',
+        'exports': 'never',
+        'functions': 'never'
+      }
+    ],
+    'no-unused-labels': 'warn',
+    'no-unused-vars': 'warn',
+    'space-before-function-paren': [
+      'error',
+      'always'
+    ],
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'no-trailing-spaces': 'error',
+    'no-multi-spaces': 'error',
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': ['error', 'never']
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import sum from './modules/sum'
 
-console.log(`Hello, world ${sum(1, 2)} ${process.env.NODE_ENV}`     )
+console.log(`Hello, world ${sum(1, 2)} ${process.env.NODE_ENV}` )
 
