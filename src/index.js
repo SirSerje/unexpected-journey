7d9d03f4f165c0743f26eddd8e002bbb7aa8a826
SirSerje <sir.serje@gmail.com>
2019-04-05 11:35:35 +0300

    #3 graph ql example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import sum from './modules/sum'
+//import sum from './modules/sum'
 
-console.log(`Hello, world ${sum(1, 2)} ${process.env.NODE_ENV}` )
+//console.log(`Hello, world ${sum(1, 2)} ${process.env.NODE_ENV}` )
 
+/*
+Connect to mongo server
+MongoClient.connect('mongodb+srv://q:XXXXXX@cluster0-cpecu.gcp.mongodb.net/test?retryWrites=true',
+  function (err, db) {
+    if(!err) {
+      console.log('We are connected')
+    }
+  })
+*/
+
+//Example of graph ql
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema.js');
+
+let port = 3000;
+const app = express();
+app.use('/', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+app.listen(port);
+console.log('GraphQL API server running at localhost:'+ port);
